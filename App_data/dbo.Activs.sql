0634a0c69e82682234dc24ae9eb3964ad0a78752
Zessho <124446033+Zessho@users.noreply.github.com>
2024-04-04 22:16:30 +0300

    AAA

---
+++ App_data/dbo.Activs.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Activs]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CabNumber] NVARCHAR(50) NOT NULL, 
+    [DepartamentID] INT NULL,
+	CONSTRAINT [FK_Activs_ToDepartaments] FOREIGN KEY ([DepartamentID])
+	REFERENCES [Departments]([Id]) ON DELETE SET NULL
+)
---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelpDeskTrain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskTrain.Controllers
+{
+    [Authorize(Roles = "Администратор, Модератор, Исполнитель")]
+    public class UserController : Controller
+    {
+        private HelpdeskContext db = new HelpdeskContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var users = db.Users.Include(u => u.Department).Include(u => u.Role).ToList();
+            return View(users);
+        }
+    }
+}
+
